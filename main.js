412cf42058ded35a0804ab8f805a9a5da14427eb
dev20223 <chingizbekturdiboyev@gmail.com>
2023-12-19 22:27:34 +0500

    6-dars bajarildi

---
+++ main.js
@@ -0,0 +1,54 @@
+//  1 masala
+// function threeNumbers(num1, num2, num3){
+//     let max;
+//     if (num1 > num2) {
+//         max = num1
+//     } else {
+//         max = num2
+//     }
+//     if (max > num3) {
+//         max = max
+//     } else {
+//         max = num3
+//     }
+//     return max
+// }
+// console.log(threeNumbers(8, 7, 21));
+
+
+// // 2masala
+
+// function determiner(n = 10){
+//     let counter = 0;
+//     for(let i = 1; i <= n; i += 2){
+//         counter += 1;
+//     }
+//     return counter
+// }
+// determiner(10)
+
+//  3 masala
+
+// function divisor(num){
+//     let counter = 1;
+//     for(let i = 1; i <= num; i++){
+//         if(num % i == 0){
+//             counter += 1
+//         }
+//     }
+//     return counter
+// }
+// console.log(divisor(100));
+
+// 4 masala
+// function cube(son) {
+//     let result;
+//     if (son % 3 == 0 && son % 5 == 0) {
+//         result = son * 2;
+//     }else
+//     {
+//         result = son ** 3;
+//     }
+//     return result
+// }
+// console.log(cube(16));
